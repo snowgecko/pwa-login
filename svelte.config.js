1fefc62f60b424aa1c5ba722cc4c677cde0ea8dd
snowgecko <kate.wykes@gmail.com>
2023-09-14 14:27:35 +0100

    updated github reference

--- svelte.config.js
+++ svelte.config.js
@@ -6,11 +6,12 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	
+	//snowgecko.github.io/pwa-login/
 	kit: {
 		adapter: adapter(),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+            base: process.env.NODE_ENV === 'production' ? '/pwa-login' : '',
         }
 	}
 };
